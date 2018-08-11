51de29db408a429a2de4edc5728b6317450d8601
Ruslan Kuprieiev <ruslan@iterative.ai>
2018-08-12 00:04:24 +0300

    test: test pip pkgs on ubuntu and fedora
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.org/iterative/dvc-test.svg?branch=master)](https://travis-ci.org/iterative/dvc-test)
+
 # dvc-test
 Integration tests for dvc
---
+++ run.py
@@ -0,0 +1,61 @@
+from __future__ import print_function
+import os
+import docker
+import platform
+
+REPO_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+test_system = os.getenv('DVC_TEST_SYSTEM', None)
+if test_system is None:
+    print("Use DVC_TEST_SYSTEM to specify test system")
+    exit(1)
+elif test_system == 'linux':
+    test_distro = os.getenv('DVC_TEST_DISTRO', None)
+    if test_distro is None:
+        print("Use DVC_TEST_DISTRO to specify test distro")
+        exit(1)
+
+    test_distro_version = os.getenv('DVC_TEST_DISTRO_VERSION', None)
+    if test_distro_version is None:
+        print("Use DVC_TEST_DISTRO_VERSION to specify test distro version")
+        exit1(1)
+
+    test_pkg = os.getenv('DVC_TEST_PKG', None)
+    if test_pkg is None:
+        print("Use DVC_TEST_PKG to specify test pkg")
+        exit(1)
+
+    docker_dir = os.path.join(REPO_ROOT,
+                              "docker",
+                              test_distro,
+                              test_distro_version)
+    client = docker.from_env()
+
+    print("Building '{}'".format(docker_dir))
+    image = client.images.build(path=docker_dir)[0]
+
+    print("Running '{}'".format(image.id))
+    con = client.containers.run(image.id,
+                                ["python", "-m", "tests"],
+                                volumes={REPO_ROOT: {'bind': '/dvc-test',
+                                                     'mode': 'rw'}},
+                                working_dir='/dvc-test',
+                                environment={"DVC_TEST_SYSTEM": test_system,
+                                             "DVC_TEST_PKG": test_pkg},
+                                auto_remove=True,
+                                detach=True)
+
+    for out in con.logs(stream=True):
+        print(out, end='')
+
+    con.wait()
+
+elif test_system == 'osx':
+    assert platform.system() == "Darwin"
+    raise NotImplementedError
+elif test_system == 'windows':
+    assert platform.system() == "Windows"
+    raise NotImplementedError
+else:
+    print("Unknown test system {}".format(test_system))
+    exit(1)
