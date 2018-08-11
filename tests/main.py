94d4ddf31b1d473e9b606ffdccf58f9e2dfbc66d
Ruslan Kuprieiev <ruslan@iterative.ai>
2018-08-12 02:55:05 +0300

    test: test *nix binary packages
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- tests/main.py
+++ tests/main.py
@@ -1,4 +1,6 @@
 import os
+import wget
+import requests
 import platform
 from subprocess import check_call
 
@@ -7,49 +9,72 @@ try:
 except:
     from pip._internal import main as pipmain
 
+
+URL = 'https://4ki8820rsf.execute-api.us-east-2.amazonaws.com/' \
+      'prod/latest-version'
+TIMEOUT = 10
+
+
+def latest_version(pkg):
+    r = requests.get(URL, timeout=TIMEOUT)
+    j = r.json()
+    return j[pkg]
+
+
+def install_latest_version(cmd, pkg):
+    latest = latest_version(pkg)
+    fname = wget.download(latest)
+    ret = os.system("{} {}".format(cmd, fname))
+    assert ret == 0
+
+
+def install_pip():
+    ret = pipmain(['install', 'dvc'])
+    assert ret == 0
+
+
+def install_deb():
+    assert platform.system() == "Linux"
+    assert platform.linux_distribution() == "Ubuntu"
+    install_latest_version('dpkg -i', 'deb')
+
+
+def install_rpm():
+    assert platform.system() == "Linux"
+    assert platform.linux_distribution() == "Fedora"
+    install_latest_version('rpm -ivh', 'rpm')
+
+
+def install_pkg():
+    assert platform.system() == "Darwin"
+    install_latest_version('installer -target / -pkg', 'pkg')
+
+def install_exe():
+    assert platform.system() == "Windows"
+    raise NotImplementedError
+
+def install():
+    pkg = os.getenv("DVC_TEST_PKG", None)
+    if pkg is None:
+        raise Exception("Use DVC_TEST_PKG to specify test package")
+    elif pkg == "pip":
+        install_pip()
+    elif pkg == "deb":
+        install_deb()
+    elif pkg == "rpm":
+        install_rpm()
+    elif pkg == "pkg":
+        install_pkg()
+    elif pkg == "exe":
+        install_exe()
+    else:
+        raise Exception("Unsupported pkg {}".format(pkg))
+
+
+
 def main(argv=None):
     ret = os.system("pip install -r requirements.txt")
     assert ret == 0
-
-    def install_pip():
-        ret = pipmain(['install', 'dvc'])
-    #    ret = os.system("pip install dvc")
-        assert ret == 0
-
-    def install_deb():
-        assert platform.system() == "Linux"
-        assert platform.linux_distribution() == "Ubuntu"
-        raise NotImplementedError
-
-    def install_rpm():
-        assert platform.system() == "Linux"
-        assert platform.linux_distribution() == "Fedora"
-        raise NotImplementedError
-
-    def install_pkg():
-        assert platform.system() == "Darwin"
-        raise NotImplementedError
-
-    def install_exe():
-        assert platform.system() == "Windows"
-        raise NotImplementedError
-
-    def install():
-        pkg = os.getenv("DVC_TEST_PKG", None)
-        if pkg is None:
-            raise Exception("Use DVC_TEST_PKG to specify test package")
-        elif pkg == "pip":
-            install_pip()
-        elif pkg == "deb":
-            install_deb()
-        elif pkg == "rpm":
-            install_rpm()
-        elif pkg == "pkg":
-            install_pkg()
-        elif pkg == "exe":
-            install_exe()
-        else:
-            raise Exception("Unsupported pkg {}".format(pkg))
 
     system = os.getenv("DVC_TEST_SYSTEM", None)
     if system is None:
