27bb7a127e0d7a34644a2a1b9179676c628a3039
Ruslan Kuprieiev <ruslan@iterative.ai>
2018-08-13 02:41:54 +0300

    test: add simple tensorflow test
    Signed-off-by: Ruslan Kuprieiev <ruslan@iterative.ai>

--- tests/simple/test.py
+++ tests/simple/test.py
@@ -6,3 +6,12 @@ class TestHelp(TestCase):
     def test(self):
         ret = os.system("dvc --help")
         self.assertEqual(ret, 0)
+
+
+class TestTensorflow(TestCase):
+    def test(self):
+        ret = os.system("python -c 'import tensorflow'")
+        self.assertEqual(ret, 0)
+
+        ret = os.system("dvc run python -c 'import tensorflow'")
+        self.assertEqual(ret, 0)
