429a86c6bce99c5c5f3a2ec23fc5e543b719340b
Ruslan Kuprieiev <kupruser@gmail.com>
2019-11-09 21:27:01 +0200

    test: win: debug

--- tests/main.py
+++ tests/main.py
@@ -77,7 +77,7 @@ def install_exe():
     
     install_latest_version('windows', '.\\{} /SP- /VERYSILENT /SUPPRESSMSGBOXES', 'exe')
 
-    ret = os.system(r"dir C:\Program Files (x86)")
+    ret = os.system(r'dir "C:\Program Files (x86)"')
     assert ret == 0
     
     ret = os.system(r'dir "C:\Program Files (x86)\Data Version Control"')
